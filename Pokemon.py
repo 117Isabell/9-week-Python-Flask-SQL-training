bd2e9002ece011fddc1f6ece371a9393b47bc125
117Isabell <isabellhepburn@outlook.com>
2024-09-14 14:10:15 +0300

    style:style the input form and pokemon data

--- Pokemon.py
+++ Pokemon.py
@@ -1,8 +1,9 @@
 import requests
 from flask import Flask, render_template, request
+import os
 from pprint import pprint as pp
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -10,7 +11,7 @@ def index():
 
 @app.route('/pokemon', methods=['POST'])
 def get_pokemon():
-    pokemon = requests.form['pokemon']
+    pokemon = request.form['pokemon']
 
 # API request
     endpoint = f'https://pokeapi.co/api/v2/pokemon/{pokemon}/'
@@ -24,14 +25,8 @@ def get_pokemon():
         height = float(data['height'])/10
         weight = float(data['weight'])/10
         image = data['sprites']['front_default']
-        print(image)
         return render_template('index.html', name=name, height=height, weight=weight, image=image)
-    else:
-        error = "Pokémon not found!"
-        return render_template('index.html', error=error)
 
-# Outputting information
-        print (f'Your Pokemon is called {name}, they are {height}m tall and weigh {weight}kgs')
 
 if __name__ == '__main__':
     app.run(debug=True)
